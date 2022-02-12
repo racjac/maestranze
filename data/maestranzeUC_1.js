397f56ae5981f7fa824f2081067f98761f89052e
racjac <62622875+racjac@users.noreply.github.com>
2022-02-12 20:38:49 +0100

    Initial commit

---
+++ data/maestranzeUC_1.js
@@ -0,0 +1,28 @@
+var json_maestranzeUC_1 = {
+"type": "FeatureCollection",
+"name": "maestranzeUC_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Cognome": "Bonini", "Nome": "Moreno", "Comune": "Fosciandora", "Squadra": "Garfagnana 12" }, "geometry": { "type": "Point", "coordinates": [ 10.452835288818848, 44.118105836022572 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Mazzei", "Nome": "Fabio", "Comune": "Pieve Fosciana", "Squadra": "Garfagnana 12" }, "geometry": { "type": "Point", "coordinates": [ 10.408632593211856, 44.129760330217032 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Rocchiccioli", "Nome": "Alvaro", "Comune": "Castiglione di Garfagnana", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.412266527888384, 44.150645807795364 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Casini", "Nome": "Guglielmo", "Comune": "Castelnuovo di Garfagnana", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.406967354736269, 44.12318297212304 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Donati", "Nome": "Giampaolo", "Comune": "San Romano in Garfagnana", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.347177056964838, 44.168904998864043 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Rocchiccioli", "Nome": "Quinto", "Comune": "San Romano in Garfagnana", "Squadra": "Garfagnana 12" }, "geometry": { "type": "Point", "coordinates": [ 10.353872308266972, 44.160517031396168 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Sarti", "Nome": "Livio", "Comune": "San Romano in Garfagnana", "Squadra": "Garfagnana 14" }, "geometry": { "type": "Point", "coordinates": [ 10.35454635236691, 44.161516883193585 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Nuccini", "Nome": "Michele", "Comune": "San Romano in Garfagnana", "Squadra": "Garfagnana 12" }, "geometry": { "type": "Point", "coordinates": [ 10.321452110842847, 44.192233365823789 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Micchi", "Nome": "Leonardo", "Comune": "Molazzana", "Squadra": "Garfagnana 12" }, "geometry": { "type": "Point", "coordinates": [ 10.394189173504088, 44.082744341758676 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Scognamiglio", "Nome": "Graziano", "Comune": "Castelnuovo di Garfagnana", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.406621438549683, 44.104143585196681 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Tolaini", "Nome": "Marco", "Comune": "Castelnuovo di Garfagnana", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.397520744811516, 44.117556525635869 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Sassi", "Nome": "Piero", "Comune": "Camporgiano", "Squadra": "Garfagnana 15" }, "geometry": { "type": "Point", "coordinates": [ 10.31786481405368, 44.156226964200805 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Biagioni", "Nome": "Michele", "Comune": "Camporgiano", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.31786017753055, 44.138460838974702 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Bosi", "Nome": "Riccardo", "Comune": "Sillano Giuncugnano", "Squadra": "Garfagnana 14" }, "geometry": { "type": "Point", "coordinates": [ 10.342128276453041, 44.238446381663941 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Pucci", "Nome": "Matteo", "Comune": "Villa Collemandina", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.394206781646695, 44.164432459736105 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Costa", "Nome": "Sara", "Comune": "Camaiore", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.318644150836928, 43.92721657805734 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Biagioni", "Nome": "Stefano", "Comune": "Castiglione di Garfagnana", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.437114349061689, 44.152058660299545 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Franchi", "Nome": "Andrea", "Comune": "Villa Collemandina", "Squadra": "Garfagnana 13" }, "geometry": { "type": "Point", "coordinates": [ 10.381539655243508, 44.153224685637539 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Abrami", "Nome": "Alberto", "Comune": "Barga", "Squadra": "Garfagnana 12" }, "geometry": { "type": "Point", "coordinates": [ 10.46543550727734, 44.069746793958764 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Ottolini", "Nome": "Luca", "Comune": "Sillano Giuncugnano", "Squadra": "Garfagnana 14" }, "geometry": { "type": "Point", "coordinates": [ 10.257048449203435, 44.218238019499069 ] } },
+{ "type": "Feature", "properties": { "Cognome": "Borghesi", "Nome": "Abramo", "Comune": "Sillano Giuncugnano", "Squadra": "Garfagnana 14" }, "geometry": { "type": "Point", "coordinates": [ 10.266660375471227, 44.230296152624632 ] } }
+]
+}
